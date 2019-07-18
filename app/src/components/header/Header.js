bd75fab19bcced231b3aba3dc269d03f95314c46
Ivan <vania149895@gmail.com>
2019-07-18 23:20:37 +0300

    Layout bug fixed

--- app/src/components/header/Header.js
+++ app/src/components/header/Header.js
@@ -1,25 +1,25 @@
-import React from 'react';
-import './style.sass';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBars, faBell } from '@fortawesome/free-solid-svg-icons';
+import React from "react";
+import style from "./style.module.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars, faBell } from "@fortawesome/free-solid-svg-icons";
 
-class Header extends React.Component 
-{ 
-  render() 
-  {  
-    return(
-      <div class="container-header">
-        <ul class="ul-header-l">
-          <li class="li-header-button"><FontAwesomeIcon icon={faBars} class="button-header"/></li>
-          <li class="li-header"><span class="title-header">BSTU.Кураторы</span></li>
-        </ul>
-        <ul class="ul-header-r">
-          <li class="li-header"><form><input id="find-form-header" type="text" value="Поиск" /></form></li>
-          <li class="li-header-button"><FontAwesomeIcon icon={faBell} class="notify-header"/></li>
-        </ul>
+const Header = () => {
+  return (
+    <div className={style.container}>
+      <div>
+        <FontAwesomeIcon icon={faBars} className={style.buttonHeader} />
+        <span className={style.title}>BSTU.Кураторы</span>{" "}
       </div>
-    );
-  }    
-} 
+      <div>
+        <input
+          className={style.findFormHeader}
+          type="text"
+          placeholder="Поиск"
+        />
+        <FontAwesomeIcon icon={faBell} className={style.notifyHeader} />
+      </div>
+    </div>
+  );
+};
 
 export default Header;
--- app/src/components/login/Login.js
+++ app/src/components/login/Login.js
@@ -1,26 +1,7 @@
-import React from 'react';
-import logo from './logo.svg';
-import './Login.css';
+import "./Login.css";
 
 function Login() {
-  return (
-    <div className="Login">
-      <header className="Login-header">
-        <img src={logo} className="Login-logo" alt="logo" />
-        <p>
-          Edit <code>src/Login.js</code> and save to reload.
-        </p>
-        <a
-          className="Login-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Login
-        </a>
-      </header>
-    </div>
-  );
+  return "LOGIN";
 }
 
 export default Login;
