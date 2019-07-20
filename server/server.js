cba55d924d815b9a2e8aff48e8d3105b3c9e4f02
vol4kovi4.v <vol4kovi4.w@yandex.ru>
2019-07-20 14:26:29 +0300

    initial

--- server/server.js
+++ server/server.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var app = express();
 
+//Sequelize
+var env = require('dotenv');
+//Models
+var models = require("./db/models");
 
 var PORT = 3000;
 app.get('/' , (req, res) => res.send('Hi world'));
@@ -10,6 +14,12 @@ app.listen(PORT , (err) =>
     console.log('Server is starting');
 });
 
+//Sync Database
+models.sequelize.sync().then(function() {
+    console.log('Nice! Database looks fine')
+}).catch(function(err) {
+    console.log(err, "Something went wrong with the Database Update!")
+});
 
 //for start server use: 1 - 'node server.js' , 2 - 'npm start' , or 3 - 'nodemon server.js'
 // '3' way allows you to refresh the page without rebooting the server
