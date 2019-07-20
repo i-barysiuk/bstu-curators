80e28ef1885dfbe83b27d097e1da019b903cfa7a
Ivan <vania149895@gmail.com>
2019-07-20 10:59:25 +0300

    config dev local

--- server/server.js
+++ server/server.js
@@ -1,15 +1,12 @@
-var express = require('express');
+var express = require("express");
 var app = express();
 
-
 var PORT = 3000;
-app.get('/' , (req, res) => res.send('Hi world'));
-app.listen(PORT , (err) => 
-{
-    if(err) return console.log('something bad happened', err);
-    console.log('Server is starting');
+app.get("/", (req, res) => res.send("Hi world"));
+app.listen(PORT, err => {
+  if (err) return console.log("something bad happened", err);
+  console.log("Server is starting");
 });
-
 
 //for start server use: 1 - 'node server.js' , 2 - 'npm start' , or 3 - 'nodemon server.js'
 // '3' way allows you to refresh the page without rebooting the server
