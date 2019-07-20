9c182adf2e68382c6ae37d2e4a2601a62d25549c
Ivan <vania149895@gmail.com>
2019-07-20 18:48:07 +0300

    migration and service and controllers

---
+++ server/services/UserService.js
@@ -0,0 +1,63 @@
+const Sequelize = require("sequelize");
+const User = require("../db/models/User");
+const Op = Sequelize.Op;
+
+class UserService {
+  get(id) {
+    return User.findOne({
+      where: {
+        id
+      }
+    });
+  }
+
+  find(user) {
+    return User.findOne({
+      where: {
+        [Op.or]: [{ email: user }, { phone: user }]
+      }
+    });
+  }
+
+  getAll() {
+    return User.findAll({});
+  }
+
+  create(body) {
+    return User.findOrCreate({
+      where: {
+        email: body.email
+      },
+      defaults: {
+        onlineAt: body.onlineAt,
+        password: body.password,
+        birthday: body.birthday,
+        sex: body.sex,
+        first_name: body.first_name,
+        last_name: body.last_name,
+        f_name: body.f_name,
+        phone: body.phone,
+        isActive: body.isActive,
+        role: body.role
+      }
+    });
+  }
+
+  update(body, id) {
+    return User.update(body, {
+      where: {
+        id
+      }
+    });
+  }
+
+  delete(id) {
+    return User.destroy({
+      where: {
+        id
+      }
+    });
+  }
+}
+
+module.exports = new UserService();
