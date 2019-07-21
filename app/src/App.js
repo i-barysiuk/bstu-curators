68afc25f263ce0f50cdadaf070fdd88fb262027c
iruseg00 <isiruseg@gmail.com>
2019-07-21 17:39:42 +0300

    F9/Menu

--- app/src/App.js
+++ app/src/App.js
@@ -3,12 +3,18 @@ import './App.css';
 import { BrowserRouter, Route } from 'react-router-dom';
 import Login from './components/login/Login';
 import Dashboard from './components/dashboard/Dashboard';
-import Header from './components/header/Header';
+import Menu from './components/menu/Menu';
 
 function App() {
   return (
     <BrowserRouter>
-      <Route exact path="/" component={Header}/>
+      <Route exact path="/" component={Menu}/>
+      <Route path="/home" component={Menu}/>
+      <Route path="/users" component={Menu}/>
+      <Route path="/idcard" component={Menu}/>
+      <Route path="/file" component={Menu}/>
+      <Route path="/calendar" component={Menu}/>
+      <Route path="/search" component={Menu}/>
       <Route path="/login" component={Login}/>
       <Route path="/dashboard" component={Dashboard}/>
     </BrowserRouter>
