465cecc4449327fa9d3a977eb56c926edc5cab84
s1ncereboy <50964716+s1ncereboy@users.noreply.github.com>
2019-07-24 04:12:13 +0300

    F12/LoginPage

--- app/src/components/login/Login.js
+++ app/src/components/login/Login.js
@@ -1,11 +1,12 @@
 import React from "react";
-import { Input, Checkbox, Button, Carousel, Icon } from "antd";
+import { Form, Input, Checkbox, Button, Carousel, Icon} from "antd";
 import { Link } from "react-router-dom";
 import "./style.css";
 import style from "./style.module.scss";
 import logo from "../../assets/images/logos/logo.png";
 
-function Login() {
+function Login(props) {
+  const { getFieldDecorator } = props.form;
   return (
     <div className={style.container}>
       <div className={style.bar}>
@@ -20,31 +21,74 @@ function Login() {
             </div>
             <div>
               <span>Test 3</span>
-            </div>
+             </div>
             <div>
               <span>Что-то прекрасное о вузе</span>
             </div>
           </Carousel>
         </div>
         <div className={style.buttons}>
-          <span className={style.mainButton}>
-            <Button type="primary" href="#">
-              <Icon type="left" />
+        <span className={style.mainButton}>
+          <Button type="primary" href="#">
+            <Icon type="left" />
               На главную
-            </Button>
-          </span>
-          <span className={style.helpButton}>
-            <Button type="primary" size={"medium"} href="#">
-              Помощь
-            </Button>
-          </span>
+          </Button>
+        </span>
+        <span className={style.helpButton}>
+          <Button type="primary" size={"medium"} href="#">
+            Помощь
+          </Button>
+        </span>
         </div>
       </div>
 
       <div className={style.rightSide}>
         <div className={style.loginForm}>
           <span className={style.textCenter}>Вход</span>
-          <Input size="large" placeholder="Email" />
+            <Form className="login-form">
+              <Form.Item>
+                {getFieldDecorator('username', {
+                  rules: [{ required: true, message: 'Введите имя пользователя!' }],
+                })(
+                  <Input
+                    size="large"
+                    prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                    placeholder="Имя пользователя"
+                  />,
+                )}
+              </Form.Item>
+              <span className={style.passAndCheckBox}>
+                <Form.Item>
+                  {getFieldDecorator('password', {
+                    rules: [{ required: true, message: 'Введите ваш пароль!' }],
+                  })(
+                    <Input.Password
+                      size="large"
+                      prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                      type="password"
+                      placeholder="Пароль"
+                    />,
+                  )}
+                </Form.Item>
+                <span className={style.boxAndForget}>
+                  <Form.Item>
+                    {getFieldDecorator('remember', {
+                      valuePropName: 'checked',
+                      initialValue: true,
+                    })(<Checkbox>Запомнить меня</Checkbox>)}
+                  </Form.Item>     
+                </span> 
+              </span>
+            </Form>  
+            <Button type="primary" href="#">
+              Войти
+            </Button>
+              или <Link to={"#"}>зарегистрироваться</Link>   
+            <Link to={"#"}>
+              забыли пароль?
+            </Link>   
+
+          {/* <Input size="large" placeholder="Email" />
           <span className={style.passAndCheckBox}>
             <Input.Password size="large" placeholder="Password" />
             <span className={style.boxAndForget}>
@@ -59,10 +103,14 @@ function Login() {
           <Button type="primary" href="#">
             Войти
           </Button>
-        </div>
-      </div>
+          <a href="">или зарегестрироваться</a>  */}
+
+        </div>  
+      </div> 
+    
     </div>
   );
 }
 
-export default Login;
+const WrappedNormalLoginForm = Form.create({ name: 'normal_login' })(Login);
+export default WrappedNormalLoginForm;
