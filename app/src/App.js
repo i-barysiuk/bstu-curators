343c785afdd202458f9949c645b7366effb07400
Ivan <vania149895@gmail.com>
2019-07-25 20:32:36 +0300

    config GA

--- app/src/App.js
+++ app/src/App.js
@@ -1,34 +1,36 @@
 import React from "react";
 import "./App.css";
-import { BrowserRouter, Route } from "react-router-dom";
+import { Router, Route } from "react-router-dom";
 import Login from "./components/login/Login";
 import Dashboard from "./components/dashboard/Dashboard";
 import Menu from "./components/menu/Menu";
-import { createBrowserHistory } from 'history';
-import ReactGA from 'react-ga';
-ReactGA.initialize("UA-144387777-1");
+import { createBrowserHistory } from "history";
+import Analitic from "./helper/analitics";
 
 const history = createBrowserHistory();
 history.listen(location => {
-  ReactGA.set({ page: location.pathname }); // Update the user's current page
-  ReactGA.pageview(location.pathname); // Record a pageview for the given page
+  Analitic.pageview(location.pathname);
 });
 
-
-function App() {
-  return (
-    <BrowserRouter history={history}>
-      <Route exact path="/" component={Menu} />
-      <Route path="/home" component={Menu} />
-      <Route path="/users" component={Menu} />
-      <Route path="/idcard" component={Menu} />
-      <Route path="/file" component={Menu} />
-      <Route path="/calendar" component={Menu} />
-      <Route path="/search" component={Menu} />
-      <Route path="/login" component={Login} />
-      <Route path="/dashboard" component={Dashboard} />
-    </BrowserRouter>
-  );
+class App extends React.Component {
+  componentDidMount() {
+    Analitic.pageview(window.location.pathname);
+  }
+  render() {
+    return (
+      <Router history={history}>
+        <Route exact path="/" component={Menu} />
+        <Route path="/home" component={Menu} />
+        <Route path="/users" component={Menu} />
+        <Route path="/idcard" component={Menu} />
+        <Route path="/file" component={Menu} />
+        <Route path="/calendar" component={Menu} />
+        <Route path="/search" component={Menu} />
+        <Route path="/login" component={Login} />
+        <Route path="/dashboard" component={Dashboard} />
+      </Router>
+    );
+  }
 }
 
 export default App;
