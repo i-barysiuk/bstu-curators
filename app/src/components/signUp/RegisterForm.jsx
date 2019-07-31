6a917a234cc6cd1ba5c86d1909fe02658e457b13
M-Kulinkovich <50986910+M-Kulinkovich@users.noreply.github.com>
2019-07-31 14:51:21 +0300

    Update RegisterForm.jsx

--- app/src/components/signUp/RegisterForm.jsx
+++ app/src/components/signUp/RegisterForm.jsx
@@ -1,21 +1,10 @@
 import React from 'react';
 import 'antd/dist/antd.css';
-import {Form, Input, Cascader, Steps, message, Col, Button, Row,} from 'antd';
+import {Form, DatePicker, Select, Input, Steps, message, Col, Button,} from 'antd';
 import style from "../loginForm/style.module.scss";
 
 const { Step } = Steps;
-const residences = [
-  {
-    value: 'Брест',
-    label: 'Брест',
-  },
-  {
-    value: 'Минск',
-    label: 'Минск',
-  }
-]
-
-
+const { Option } = Select;
 class RegistrationForm extends React.Component {
   constructor(props) {
     super(props);
@@ -23,6 +12,25 @@ class RegistrationForm extends React.Component {
       current: 0,
     };
   }
+  compareToFirstPassword = (rule, value, callback) => {
+    const { form } = this.props;
+    if (value && value !== form.getFieldValue('Пароль')) {
+      callback('Пароли не совпадают!');
+    } else {
+      callback();
+    }
+  };
+  handleConfirmBlur = e => {
+    const { value } = e.target;
+    this.setState({ confirmDirty: this.state.confirmDirty || !!value });
+  };
+  validateToNextPassword = (rule, value, callback) => {
+    const { form } = this.props;
+    if (value && this.state.confirmDirty) {
+      form.validateFields(['confirm'], { force: true });
+    }
+    callback();
+  };
   prev() {
     const current = this.state.current - 1;
     this.setState({ current });
@@ -36,41 +44,30 @@ class RegistrationForm extends React.Component {
   render() {
     const { current } = this.state;
     const { getFieldDecorator } = this.props.form;
-    const formItemLayout = {
-      labelCol: {
-        xs: { span: 24 },
-        sm: { span: 8 },
-      },
-      wrapperCol: {
-        xs: { span: 24 },
-        sm: { span: 16 },
-      },
-    };
     const steps = [
       {
         title: 'Основное',
         content : 
         <Form className = {style.FirstStep}>
-        <Form {...formItemLayout} onSubmit={this.handleSubmit}></Form>
         <Form.Item label="Фамилия" size = "large">
         <Col span={24}>
           {getFieldDecorator('Фамилия', {
             rules: [{ required: true, message: 'Введите свою фамилию!', whitespace: true }],
-          })(<Input  />)}
+          })(<Input placeholder="Введите свою фамилию"/>)}
           </Col>
         </Form.Item>
         <Form.Item label="Имя">
         <Col span={24}>
           {getFieldDecorator('Имя', {
                         rules: [{ required: true, message: 'Введите своё Имя!', whitespace: true }],
-          })(<Input />)}
+          })(<Input placeholder="Введите своё имя" />)}
           </Col>
         </Form.Item>
         <Form.Item label="Отчетсво">
         <Col span={24}>
           {getFieldDecorator('Отчество', {
                         rules: [{ required: true, message: 'Выберите дату рождения!', whitespace: true }],
-          })(<Input style={{ width: 300 }}/>)}
+          })(<Input placeholder="Введите свое отчество" style={{ width: 300 }}/>)}
           </Col>
         </Form.Item> 
         </Form>
@@ -80,38 +77,82 @@ class RegistrationForm extends React.Component {
         content :
         <Form>
         <Form.Item label="Пол:" >
-            <Col span={24}>
             <Button>Мужской</Button>
               <Button style = {{marginLeft: "5px"}}>Женский</Button>
-            </Col>
         </Form.Item>
-      <Form.Item label="Дата рождения:">
-      <Col span={24}>
-        {getFieldDecorator('Дата рождения', {
-                      rules: [{ required: true, message: 'Выберите дату рождения!', whitespace: true }],
-        })(<Input  style={{ width: 300 }}/>)}
-        </Col>
-      </Form.Item> 
-      <Form.Item label="Город: ">
-       <Col span={24}>
-          {getFieldDecorator('Город', {
-            initialValue: ['', 'Брест', 'Минск'],
-            rules: [
-              { required: true, message: 'Выберете ваш город!' },
-            ],
-          })(<Cascader options={residences} />)}
-       </Col>
-      </Form.Item>
+        <Form.Item label="Дата рождения" >
+          {getFieldDecorator('Выберите дату рождения', {
+            rules: [{ required: true, message: 'Выберите дату рождения!' }],
+          })(<DatePicker style={{ width: 300 }} />)}
+        </Form.Item>
+        <Form.Item label="Город" hasFeedback>
+          {getFieldDecorator('Выберете город', {
+            rules: [{ required: true, message: 'Выберете свой город!' }],
+          })(
+            <Select placeholder="Выберете свой город!">
+              <Option value="Брест">Брест</Option>
+              <Option value="Минск">Минск</Option>
+            </Select>,
+          )}
+        </Form.Item>
     </Form>
       },
       {
         title: 'Контакты',
+        content :
+        <Form>
+                  <Form.Item label="Электронная почта">
+          {getFieldDecorator('Электронная почта', {
+            rules: [
+              {
+                type: 'email',
+                message: 'Некорректная электронная почта!',
+              },
+              {
+                required: true,
+                message: 'Введите свою электронную почту!',
+              },
+            ],
+          })(<Input placeholder="Введите электронную почту" style={{ width: 300 }}/>)}
+        </Form.Item>
+        <Form.Item label="Телефон">
+          {getFieldDecorator('Телефон', {
+            rules: [{ required: true, message: 'Введите свой номер телефона!' }],
+          })(<Input placeholder="Введите свой номер телефона"/>)}
+        </Form.Item>
+        <Form.Item label="Пароль" hasFeedback>
+          {getFieldDecorator('Пароль', {
+            rules: [
+              {
+                required: true,
+                message: 'Придумайте пароль!',
+              },
+              {
+                validator: this.validateToNextPassword,
+              },
+            ],
+          })(<Input.Password placeholder="Введите пароль"/>)}
+        </Form.Item>
+        <Form.Item label="Подтвердите пароль" hasFeedback>
+          {getFieldDecorator('confirm', {
+            rules: [
+              {
+                required: true,
+                message: 'Пожалуйста подтвердите свой пароль!',
+              },
+              {
+                validator: this.compareToFirstPassword,
+              },
+            ],
+          })(<Input.Password onBlur={this.handleConfirmBlur} placeholder="Подтвердите пароль" />)}
+        </Form.Item>
+        </Form>
         
       },
     ];
     return (
-      <div>
-      <Steps  current={current}>
+      <div className={style.s}>
+      <Steps  size = "small" current={current}>
         {steps.map(item => (
           <Step key={item.title} title={item.title} />
         ))}
@@ -119,19 +160,19 @@ class RegistrationForm extends React.Component {
       <div className={style.steps}>
         <div className={style.stepsContent}>{steps[current].content}</div>
         <div className={style.stepsAction}>
-          {current === steps.length - 1 && (
-            <Button type="primary" onClick={() => message.success('Вы успешно зарегестрировались!')}>
-              Зарегистрироваться
-            </Button>
-          )}
           {current > 0 && (
             <Button style={{ marginLeft: 8 }} onClick={() => this.prev()}>
               Назад
             </Button>
           )}
                     {current < steps.length - 1 && (
-            <Button style={{ marginLeft: 100 }} type="primary" onClick={() => this.next()}>
+            <Button style={{ marginLeft: 200 }} type="primary" onClick={() => this.next()}>
               Далее
+            </Button>
+          )}
+          {current === steps.length - 1 && (
+            <Button type="primary" style={{ marginLeft: 80 }} onClick={() => message.success('Вы успешно зарегестрировались!')}>
+              Зарегистрироваться
             </Button>
           )}
         </div>
