0b275f3232c8bed7d9e220b055a2861c1173d913
M-Kulinkovich <50986910+M-Kulinkovich@users.noreply.github.com>
2019-07-31 12:26:32 +0300

    Update analitics.js

--- app/src/helper/analitics.js
+++ app/src/helper/analitics.js
@@ -1,4 +1,4 @@
-import ReactGA from "react-ga";
-ReactGA.initialize("UA-144387777-1");
+// import ReactGA from "react-ga";
+// ReactGA.initialize("UA-144387777-1");
 
-export default ReactGA;
+// export default ReactGA;
