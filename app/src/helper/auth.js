c97de68dfd6fa566a6dce17871129cecf4e044a3
Ivan <vania149895@gmail.com>
2019-08-04 14:16:53 +0300

    env fix

--- app/src/helper/auth.js
+++ app/src/helper/auth.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
 if (process.env.REACT_APP_ENV === "production")
-  axios.defaults.baseURL = "server/api/";
+  axios.defaults.baseURL = "http://185.66.71.54:8001/api/";
 if (process.env.REACT_APP_ENV === "test")
-  axios.defaults.baseURL = "server_uat/api/";
+  axios.defaults.baseURL = "http://185.66.71.54:8011/api/";
 if (process.env.REACT_APP_ENV === "development")
   axios.defaults.baseURL = "http://localhost:8000/api/";
 
