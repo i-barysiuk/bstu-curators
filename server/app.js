f230cb4426e8b29b2c252b525814664c1335b0a1
s1ncereboy <50964716+s1ncereboy@users.noreply.github.com>
2019-08-07 18:31:50 +0300

    Added MailService

--- server/app.js
+++ server/app.js
@@ -13,6 +13,8 @@ const GroupController = require("./controllers/group/GroupController");
 
 const UserService = require("./services/UserService");
 const GroupService = require("./services/GroupService");
+const MailService = require("./services/MailService");
+
 
 UserService.create({
   email: "admin@care.webdad.by",
