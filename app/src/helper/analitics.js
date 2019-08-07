628337d0fe254f4c0e7a1eba89c9098b13a356a4
Mishagitwork <vecktormp1@gmail.com>
2019-08-07 19:49:20 +0300

    update branch:

--- app/src/helper/analitics.js
+++ app/src/helper/analitics.js
@@ -1,4 +1,9 @@
 import ReactGA from "react-ga";
-ReactGA.initialize("UA-144387777-1");
+ReactGA.initialize("UA-144387777-1",{
+    debug: true,
+    gaOptions: {
+        userId: 123
+    }
+  });
 
 export default ReactGA;
