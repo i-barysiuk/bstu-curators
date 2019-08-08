fb2748473cbca0da5a88660435e18445d834d633
Ivan <vania149895@gmail.com>
2019-08-08 13:36:30 +0300

    some minor changes

--- app/src/App.js
+++ app/src/App.js
@@ -17,7 +17,6 @@ import PageWrapper from "./containers/pageWrapper/PageWrapper";
 import { logout } from "./redux/actions/auth";
 import { whoAmI } from "./redux/actions/users";
 
-
 history.listen(location => {
   Analitic.pageview(location.pathname);
 });
