24bf91508ca10a5297c79b8114317a2783e82522
M-Kulinkovich <50986910+M-Kulinkovich@users.noreply.github.com>
2019-08-10 14:15:57 +0300

    Update Tables.js

--- app/src/components/studentTables/Tables.js
+++ app/src/components/studentTables/Tables.js
@@ -4,7 +4,6 @@ import 'antd/dist/antd.css';
 import style from "./style.module.scss";
 import { Table, Button, Popover, Icon,} from 'antd';
 
-
 const data = [
   {
     first_name: 'sadasd`',
@@ -25,55 +24,56 @@ const data = [
     email: "eawfasf@mail.ru",
   },
 ];
-const columns = [
-  {
-    title: 'Имя',
-    dataIndex: 'first_name',
-  },
-  {
-    title: 'Фамилия',
-    dataIndex: 'last_name',
-  },
-  {
-    title: 'Отчетсво',
-    dataIndex: 'f_name',
-  },
-  {
-    title: 'Пол',
-    dataIndex: 'sex',
-    align: 'center',
-    render : () => (
-      <Icon type="man" />
-    )
-    // render: (callback) => {
-    //   if(data.email !== 'woman'){
-    //     callback(<Icon type="man" />);
-    //   }
-    //   else {
-    //     callback(<Icon type="woman" />);
-    //   }
-    // }
-  },
-  {
-    title: 'Дата рождения',
-    dataIndex: 'birthday',
-  },
-  {
-    title: 'Телефон',
-    dataIndex: 'phone',
-  },
-  {
-    title: 'Электронная почта',
-    dataIndex: 'email',
-    align: 'center',
-    render: () => (
-      <Popover content={data.email} >
-      <Button icon="mail" type="primary"></Button>
-    </Popover> 
-    )
-  },
-];
 function Table1(){
+  const columns = [
+    {
+      title: 'Имя',
+      dataIndex: 'first_name',
+    },
+    {
+      title: 'Фамилия',
+      dataIndex: 'last_name',
+    },
+    {
+      title: 'Отчетсво',
+      dataIndex: 'f_name',
+    },
+    {
+      title: 'Пол',
+      dataIndex: 'sex',
+      align: 'center',
+      render: (data) => {
+        if(data.sex === 'woman'){
+          return (
+            <Icon type="woman" />
+          )
+        }
+        else {
+          return (
+            <Icon type="man" />
+          )
+        }
+      }
+    },
+    {
+      title: 'Дата рождения',
+      dataIndex: 'birthday',
+    },
+    {
+      title: 'Телефон',
+      dataIndex: 'phone',
+    },
+    {
+      title: 'Электронная почта',
+      dataIndex: 'email',
+      align: 'center',
+      render: (data) => (
+        <Popover content={data.email} >
+        <Button icon="mail" type="primary"></Button>
+      </Popover> 
+      )
+    },
+  ];
 return(
 <div className={style.container}>
  <div className={style.buttonsAndHeadline}>
@@ -82,15 +82,15 @@ return(
                 <Button type="primary" icon="plus" className={style.buttonShadow} />    
                </div>
             </div>
-            <div className={style.content}>
-            <Table
-    size="middle"
-    columns={columns}
-    dataSource={data}
-    bordered
-  />
-  </div>
-            </div>
+             <div className={style.content}>
+              <Table
+               size="middle"
+               columns={columns}
+               dataSource={data}
+               bordered
+              />
+             </div>
+</div>
 
 );
 }
