b7011b103e5345f0ec1bff11898df6e31c4ef054
Arseniy <isiruseg@gmail.com>
2019-08-13 15:15:07 +0300

    F50/EventCard

--- app/src/App.js
+++ app/src/App.js
@@ -19,6 +19,7 @@ import PageWrapper from "./containers/pageWrapper/PageWrapper";
 
 import { logout } from "./redux/actions/auth";
 import { whoAmI } from "./redux/actions/users";
+import EventCard from './components/eventCard/EventCard';
 
 history.listen(location => {
   Analitic.pageview(location.pathname);
@@ -40,6 +41,13 @@ class App extends React.Component {
           path="/"
           render={props => (
             <PageWrapper {...props} title="Главная" component={Welcom} />
+          )}
+        />
+        <Route
+          exact
+          path="/asd"
+          render={props => (
+            <PageWrapper {...props} title="Главная" component={EventCard} />
           )}
         />
         <Route
--- app/src/components/eventCard/EventCard.jsx
+++ app/src/components/eventCard/EventCard.jsx
@@ -19,94 +19,41 @@ import {
   faChevronRight
 } from "@fortawesome/free-solid-svg-icons";
 
-class EventCard extends React.Component
+export default props=>
 {
-  constructor(props)
-  {
-    super(props);
-    this.state = {icon : null};
-  }
-
-  newState()
-  {
-      switch(this.props.icon)
-      {
-        case 'Аттестация':
-            this.setState( {icon: faDraftingCompass});
-              break;
-        case 'Каникулы':
-            this.setState( {icon: faUmbrellaBeach});
-              break;
-        case 'Экзамен':
-            this.setState( {icon: faMeteor});
-              break;
-        case 'Военный праздник':
-            this.setState( {icon: faMeteor});
-              break;
-        case 'Зачисление студента':
-            this.setState( {icon: faUserPlus});
-              break;
-        case 'Отчисление студента':
-            this.setState( {icon: faUserTimes});
-              break; 
-        case 'Культурно-массовое мероприятие':
-            this.setState( {icon: faTheaterMasks});
-              break; 
-        case 'Донорство':
-            this.setState( {icon: faHeartbeat});
-              break;
-        case 'Спортивное мероприятие':
-            this.setState( {icon: faTableTennis});
-              break;  
-        case 'Субботник':
-            this.setState( {icon:  faSnowplow});
-              break;
-        case 'Отработка':
-            this.setState( {icon: faChalkboardTeacher});
-              break; 
-        case 'Пересдача экзамена':
-            this.setState( {icon: faAtlas});
-              break; 
-        case 'Линейка':
-            this.setState( {icon: faChild});
-              break; 
-        case 'Собрание':
-            this.setState( {icon: faUsers});
-              break; 
-        case 'Кураторский час':
-            this.setState( {icon: faSchool});
-              break;                  
-        default:  
-            this.setState( {icon:  faSchool});
-          break;
-    }
-  }
-
-  componentDidMount() 
-  {
-    this.newState();
-  }
-
-  render()
-  {
-    return ( 
-      <div className={style.container} 
-          onClick={() => {
-          if (this.props.onClick) this.props.onClick();
-          }}>
-          <FontAwesomeIcon icon={this.state.icon} className={style.icon} />
-          <div className={style.time}>
-            <div className={style.clock}>{this.props.time.hours}:{this.props.time.mins}</div>
-            <div className={style.day}>{this.props.time.day}  {this.props.time.month}</div>
-          </div>
-          <div className={style.info}>
-            <div className={style.header}>{this.props.header}</div>
-            <div className={style.subHeader}>{this.props.subHeader}</div>
-          </div>
-          <FontAwesomeIcon icon={faChevronRight} className={style.icon} />
-      </div>
-     );
-  }
+  var icons = [
+    {id: 'Аттестация'                      , value: faDraftingCompass  },
+    {id: 'Каникулы'                        , value: faUmbrellaBeach    },
+    {id: 'Экзамен'                         , value: faMeteor           },
+    {id: 'Зачисление студента'             , value: faUserPlus         },
+    {id: 'Отчисление студента'             , value: faUserTimes        },
+    {id: 'Культурно-массовое мероприятие'  , value: faTheaterMasks     },
+    {id: 'Донорство'                       , value: faHeartbeat        },
+    {id: 'Спортивное мероприятие'          , value: faTableTennis      },
+    {id: 'Субботник'                       , value: faSnowplow         },
+    {id: 'Отработка'                       , value: faChalkboardTeacher},
+    {id: 'Пересдача экзамена'              , value: faAtlas            },
+    {id: 'Линейка'                         , value: faChild            },
+    {id: 'Собрание'                        , value: faUsers            },
+    {id: 'Кураторский час'                 , value: faSchool           },
+    {id: 'Военный праздник'                , value: faMeteor           },
+   ];
+  var icon = icons.find(item => item.id === props.event);
+  return ( 
+    <div className={style.container} 
+        onClick={() => {
+        if (props.onClick) props.onClick();
+        }}>
+        <FontAwesomeIcon icon={icon.value} className={style.icon} />
+        <div className={style.time}>
+          <div className={style.clock}>{props.time.hours}:{props.time.mins}</div>
+          <div className={style.day}>{props.time.day}  {props.time.month}</div>
+        </div>
+        <div className={style.info}>
+          <div className={style.header}>{props.header}</div>
+          <div className={style.subHeader}>{props.subHeader}</div>
+        </div>
+        <FontAwesomeIcon icon={faChevronRight} className={style.icon} />
+    </div>
+   );
 }
-
-export default EventCard;
