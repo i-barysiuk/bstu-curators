56484e9cd173eb8b63e73a8bb4541f77a179d470
nikitaz1 <nikita_z@bk.ru>
2019-08-13 18:30:34 +0300

    fetch active group

--- app/src/redux/reducers/grops.js
+++ app/src/redux/reducers/grops.js
@@ -1,13 +1,28 @@
-import { GROUPS_SUCCESS, GROUPS_FAILED } from "../actionsTypes/groups";
+import {
+  GROUPS_SUCCESS,
+  GROUPS_FAILED,
+  GROUPS__ACTIVE_SUCCESS,
+  GROUPS__ACTIVE_FAILED
+} from "../actionsTypes/groups";
 
-export const defaultGroupsState = { active: {}, all: [] };
+export const defaultGroupsState = {
+  active: {},
+  all: {},
+  archive: {},
+  my: [],
+  favorite: []
+};
 
 export default (state = defaultGroupsState, action) => {
   switch (action.type) {
     case GROUPS_SUCCESS:
-      return { ...state, all: action.payload };
+      return { ...state, ...action.payload };
+
+    case GROUPS__ACTIVE_SUCCESS:
+      return { ...state, active: action.payload };
 
     case GROUPS_FAILED:
+    case GROUPS__ACTIVE_FAILED:
     default:
       return state;
   }
