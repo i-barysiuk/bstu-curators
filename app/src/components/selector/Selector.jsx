adecbb5e5170433ca6f0f112acf46e486ea9ce64
Billerens <degtyarikilya@yandex.ru>
2019-08-20 13:50:16 +0300

    Added selector for students of curators

---
+++ app/src/components/selector/Selector.jsx
@@ -0,0 +1,63 @@
+import React from "react";
+import { Select, Avatar } from "antd";
+import StudentService from "../../services/StudentService";
+import UserService from "../../services/UsersService";
+
+const { Option } = Select;
+
+export default class Selector extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: []
+    };
+  }
+
+  chooseData() {
+    this.props.isStudent
+      ? this.setState({ data: StudentService.getAll() })
+      : UserService.getAllCurators().then(res => {
+          this.setState({
+            data: res.data
+          });
+        });
+  }
+
+  componentDidMount() {
+    this.chooseData();
+  }
+
+  render() {
+    console.log(this.state.data);
+    return (
+      <Select
+        size={"large"}
+        showSearch
+        style={{ width: "100%" }}
+        placeholder={
+          this.props.isStudent
+            ? "Начните вводить ФИО студента или его группу..."
+            : "Начните вводить ФИО куратора или его кафедру..."
+        }
+        optionFilterProp="children"
+      >
+        {this.state.data.map((item, index) => {
+          return (
+            <Option key={index} style={{ display: "flex" }}>
+              <Avatar size={"small"} style={{ marginRight: "15px" }}>
+                {item.initials}
+              </Avatar>
+              {item.first_name + " " + item.last_name} {item.f_name}
+              <br />
+              {this.props.isStudent
+                ? "Группа " + item.group
+                : "Кафедра: " + item.cathedra}
+              <br />
+              {item.phone ? "Телефон: " + item.phone : null}
+            </Option>
+          );
+        })}
+      </Select>
+    );
+  }
+}
