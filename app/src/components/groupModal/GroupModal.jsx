0394f365de430acde7cdd17f840783e102ce779a
Vadim <47842271+vol4k@users.noreply.github.com>
2019-09-12 22:47:56 +0300

    Update GroupModal.jsx

--- app/src/components/groupModal/GroupModal.jsx
+++ app/src/components/groupModal/GroupModal.jsx
@@ -245,7 +245,7 @@ class GroupForm extends React.Component {
         className={style.modal}
         destroyOnClose={true}
         maskClosable={false}
-        onCancel={this.onCancel}
+        onCancel={this.onClose()}
         visible={isOpen}
         okText={"Сохранить"}
         cancelText={"Отмена"}
