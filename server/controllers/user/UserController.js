c2eb0049de996d4d17c9230b3169df4c21690e3a
M-Kulinkovich <wait_z@mail.ru>
2019-10-07 07:26:20 +0300

    Update UserController.js

--- server/controllers/user/UserController.js
+++ server/controllers/user/UserController.js
@@ -4,62 +4,68 @@ const router = express.Router();
 const UserService = require("../../services/UserService");
 
 router.get("/", (req, res) => {
-  UserService.getAll()
-    .then(data => res.status(200).json(data))
-    .catch(err => console.error(err));
+    UserService.getAll()
+        .then(data => res.status(200).json(data))
+        .catch(err => console.error(err));
 });
 
 router.get("/me", (req, res) => {
-  UserService.get(req.user.id)
-    .then(data => res.status(200).json(data))
-    .catch(err => console.error(err));
+    UserService.get(req.user.id)
+        .then(data => res.status(200).json(data))
+        .catch(err => console.error(err));
 });
 
 router.get("/:id", (req, res) => {
-  UserService.get(req.params.id)
-    .then(data => res.status(200).json(data))
-    .catch(err => console.error(err));
+    UserService.get(req.params.id)
+        .then(data => res.status(200).json(data))
+        .catch(err => console.error(err));
 });
 
 router.post("/", (req, res) => {
-  UserService.create(req.body)
-    .then(user => res.status(201).json(user))
-    .catch(err => console.error(err));
+    UserService.create(req.body)
+        .then(user => res.status(201).json(user))
+        .catch(err => console.error(err));
 });
 
 router.post("/like", (req, res) => {
-  console.log(req.body.value);
-  UserService.getAllLike(req.body.value)
-    .then(data => res.status(200).json(data))
-    .catch(err => console.error(err));
+    console.log(req.body.value);
+    UserService.getAllLike(req.body.value)
+        .then(data => res.status(200).json(data))
+        .catch(err => console.error(err));
 });
 
 router.put("/:id", (req, res) => {
-  UserService.update(req.body, req.params.id)
-    .then(updatedRecord => {
-      if (updatedRecord[0] === 1) {
-        res.status(200).json({ message: "Updated successfully" });
-      } else {
-        res.status(404).json({ message: "record not found" });
-      }
-    })
-    .catch(error => {
-      res.status(500).json(error);
-    });
+    UserService.update(req.body, req.params.id)
+        .then(updatedRecord => {
+            if (updatedRecord[0] === 1) {
+                res.status(200).json({ message: "Updated successfully" });
+            } else {
+                res.status(404).json({ message: "record not found" });
+            }
+        })
+        .catch(error => {
+            res.status(500).json(error);
+        });
 });
 
 router.delete("/:id", (req, res) => {
-  UserService.delete(req.params.id)
-    .then(deletedRecordCount => {
-      if (deletedRecordCount === 1) {
-        res.status(200).json({ message: "Deleted successfully" });
-      } else {
-        res.status(404).json({ message: "record not found" });
-      }
-    })
-    .catch(error => {
-      res.status(500).json(error);
-    });
+    UserService.delete(req.params.id)
+        .then(deletedRecordCount => {
+            if (deletedRecordCount === 1) {
+                res.status(200).json({ message: "Deleted successfully" });
+            } else {
+                res.status(404).json({ message: "record not found" });
+            }
+        })
+        .catch(error => {
+            res.status(500).json(error);
+        });
+});
+
+router.post("/setAvatar/:id", (req, res) => {
+    UserService.setImageBase64(req.body)
+        .then(user => res.status(201).json(user))
+        .catch(err => console.error(err));
 });
 
 module.exports = router;
