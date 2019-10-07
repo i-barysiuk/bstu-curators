3ad8e525928cb4fadd8d97fc95041a6cee5cf3a9
M-Kulinkovich <wait_z@mail.ru>
2019-10-07 07:26:18 +0300

    Update UsersService.js

--- app/src/services/UsersService.js
+++ app/src/services/UsersService.js
@@ -1,14 +1,18 @@
 import api from "../helper/api";
 
 class UserService {
-  whoAmI() {
-    return api("/users/me");
-  }
+    whoAmI() {
+        return api("/users/me");
+    }
 
-  getAllLike(like) {
-    console.log(like);
-    return api("/users/like", "POST", { value: like });
-  }
+    getAllLike(like) {
+        console.log(like);
+        return api("/users/like", "POST", { value: like });
+    }
+
+    setAvatar(id, imageUrl) {
+        return api(`/users/setAvatar/${id}`, "POST", { imageUrl: imageUrl });
+    }
 }
 
 export default new UserService();
