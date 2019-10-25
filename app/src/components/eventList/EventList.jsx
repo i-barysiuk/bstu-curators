86b99d232267f8d2896404dac67fe2d1da10fde3
Mishagitwork <vecktormp1@gmail.com>
2019-10-25 21:05:42 +0300

    sort by curent time

--- app/src/components/eventList/EventList.jsx
+++ app/src/components/eventList/EventList.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import style from "./style.module.scss";
 import { connect } from "react-redux";
+import { Radio } from "antd";
 import EventCard from "../eventCard/EventCard";
 import Card from "../common/card/Card";
 import BigButton from "../common/bigButton/BigButton";
@@ -14,7 +15,8 @@ class EventList extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      data: []
+      data: [],
+      sortData: []
     };
   }
 
@@ -24,17 +26,74 @@ class EventList extends React.Component {
         var eventsData = response.data.map(item => {
           return { key: item.id, ...item.event };
         });
-        this.setState({ data: eventsData });
+        this.setState({ data: eventsData, sortData: eventsData });
       });
     }
   }
+
+  sortEvent = data => {
+    var curentDate = new Date();
+    switch (data) {
+      case "ALL":
+        this.setState({ sortData: this.state.data });
+        break;
+      case "PAST":
+        var pastEvent = this.state.data.filter(item => {
+          if (curentDate > item.dataStart) {
+            return { ...item };
+          }
+          return 0;
+        });
+        this.setState({ sortData: pastEvent });
+        break;
+      case "ACTIVE":
+        var activeEvent = this.state.data.filter(item => {
+          if (curentDate < item.dataStart) {
+            return { ...item };
+          }
+          return 0;
+        });
+        this.setState({ sortData: activeEvent });
+        break;
+      default:
+        return this.setState({ sortData: this.state.data });
+    }
+  };
 
   render() {
     return (
       <Card
         title={"События"}
         buttons={[
-          <BigButton icon={faFilter} onClick={this.props.onClick} />,
+          <BigButton
+            icon={faFilter}
+            dropdown
+            content={
+              <div>
+                <Radio.Group defaultValue={"ALL"}>
+                  <Radio.Button
+                    value={"ALL"}
+                    onClick={() => this.sortEvent("ALL")}
+                  >
+                    Все
+                  </Radio.Button>
+                  <Radio.Button
+                    value={"PAST"}
+                    onClick={() => this.sortEvent("PAST")}
+                  >
+                    Прошедшие
+                  </Radio.Button>
+                  <Radio.Button
+                    value={"ACTIVE"}
+                    onClick={() => this.sortEvent("ACTIVE")}
+                  >
+                    Актуальные
+                  </Radio.Button>
+                </Radio.Group>
+              </div>
+            }
+            onClick={this.props.onClick}
+          />,
           <BigButton
             icon={faPlus}
             primary
@@ -44,7 +103,7 @@ class EventList extends React.Component {
         contentCenter
       >
         <div className={style.cards}>
-          {this.state.data.map((item, index) => {
+          {this.state.sortData.map((item, index) => {
             item.dataStart = moment(item.dataStart);
             return (
               <div className={style.item} key={item.id}>
